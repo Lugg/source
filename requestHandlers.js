d865e92963b112f4dbe5468cb4e981f29605cb16
LUGG <lukas.rust@gmail.com>
2014-03-04 23:46:39 +0100

    added error handling for the nodejs server

--- requestHandlers.js
+++ requestHandlers.js
@@ -31,13 +31,31 @@ function show(response, query) {
 	
 	if(Path== null)
 	{
-		
+		response.writeHead(200, {"Content-Type": "text/plain"})
+    	response.write(" 404 no Path specified");
+    	response.end();
 	}
 	else
 	{
-		console.log("hallo");
-		response.writeHead(200, {"Content-Type": "image/png"});
-            fs.createReadStream("./"+Path).pipe(response);
+         // This line opens the file as a readable stream
+  		var readStream = fs.createReadStream("./"+Path);
+
+  		// This will wait until we know the readable stream is actually valid before piping
+  		readStream.on('open', function () {
+    	// This just pipes the read stream to the response object (which goes to the client)
+    		response.writeHead(200, {"Content-Type": "image/png"});
+    		readStream.pipe(res);
+  		});
+
+  		// This catches any errors that happens while creating the readable stream (usually invalid names)
+  		readStream.on('error', function(err) {
+    	
+		console.log();    		
+		response.writeHead(200, {"Content-Type": "text/plain"})
+    	response.write(" 404 Ressource named "+Path+" not found");
+    	response.end();
+
+  		});
 		
 	}
 }
