375f2e4d467b43daaa2b8c02f8d707964bdc9b37
LUGG <lukas.rust@gmail.com>
2014-03-05 00:35:04 +0100

    resolved jsHint server warnings

--- requestHandlers.js
+++ requestHandlers.js
@@ -12,7 +12,7 @@ response.writeHead(200, {"Content-Type": "text/html"});
 response.write("<DOCTYPE HTML>");
 response.write("<html><body>");
 response.write(stdout);
-response.write("</body></html>")
+response.write("</body></html>");
 response.end();
 });
 }
@@ -25,37 +25,36 @@ response.end();
 }
 
 function show(response, query) {
-	var Path = querystring.parse(query)["imgName"];
+	var Path = querystring.parse(query).imgName;
 	console.log("Request handler 'show' was called");
 	console.log("request eq " + query);
 	
-	if(Path== null)
+	if(Path===null)
 	{
-		response.writeHead(200, {"Content-Type": "text/plain"})
-    	response.write(" 404 no Path specified");
-    	response.end();
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("404 no Path specified");
+    response.end();
 	}
 	else
 	{
          // This line opens the file as a readable stream
-  		var readStream = fs.createReadStream("./"+Path);
+         var readStream = fs.createReadStream("./"+Path);
 
-  		// This will wait until we know the readable stream is actually valid before piping
-  		readStream.on('open', function () {
-    	// This just pipes the read stream to the response object (which goes to the client)
-    		response.writeHead(200, {"Content-Type": "image/png"});
-    		readStream.pipe(res);
-  		});
+        // This will wait until we know the readable stream is actually valid before piping
+        readdStream.on('open', function () {
+        //This just pipes the read stream to the response object (which goes to the client)
+        response.writeHead(200, {"Content-Type": "image/png"});
+        readStream.pipe(res);
+      });
 
-  		// This catches any errors that happens while creating the readable stream (usually invalid names)
-  		readStream.on('error', function(err) {
-    	
-		console.log();    		
-		response.writeHead(200, {"Content-Type": "text/plain"})
-    	response.write(" 404 Ressource named "+Path+" not found");
-    	response.end();
+      // This catches any errors that happens while creating the readable stream (usually invalid names)
+      readStream.on('error', function(err) {
 
-  		});
+		response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(" 404 Ressource named "+Path+" not found");
+    response.end();
+
+  });
 		
 	}
 }
